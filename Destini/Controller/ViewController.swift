cc7ee2c41dd4343575eb66eb16677184af9f48e8
alexSoftDeveloper <pm.alex.med@gmail.com>
2024-09-28 11:56:22 +0700

    C1 flow work

--- Destini/Controller/ViewController.swift
+++ Destini/Controller/ViewController.swift
@@ -14,11 +14,37 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+  
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI(count)
     }
-
-
+    
+    @IBAction func choiceMade(_ sender: UIButton) {
+        
+        switch sender.titleLabel?.text {
+        case story[3].choice1 :
+            count = 4
+        case story[3].choice2 :
+            count = 0
+        case story[0].choice1 :
+            count = 1
+        case story[0].choice2 :
+            count = 2
+        case story[1].choice1 :
+            count = 3
+        case story[1].choice2 :
+            count = 3
+        default: print("Stop")
+        }
+        viewDidLoad()
+    }
+    func updateUI(_ nextQuestion: Int) {
+        storyLabel.text = story[nextQuestion].title
+        choice1Button.setTitle(story[nextQuestion].choice1, for: .normal)
+        choice2Button.setTitle(story[nextQuestion].choice2, for: .normal)
+    }
+    
 }
 
--- Destini/Model/Story.swift
+++ Destini/Model/Story.swift
@@ -7,3 +7,21 @@
 //
 
 import Foundation
+
+struct Story {
+    var title: String
+    var choice1: String
+    var choice2: String
+    
+    init(t: String, c1: String, c2: String) {
+        title = t
+        choice1 = c1
+        choice2 = c2
+    }
+}
+
+let story = [Story(t: "You see a fork in the road.", c1: "Take a left.", c2: "Take a right."), Story(t: "You see a tiger.", c1: "Shout for help.", c2: "Play dead"), Story(t: "You find a treasure chest.", c1: "opn it.", c2: "check for traps."), Story(t: "You have destinaion! Well done!!", c1: "Exit", c2: "Repeat"), Story(t: "FINAL!", c1: "", c2: "")]
+var story0 = story[0]
+var question = 0
+var numberQuestion = 0
+var count = 0
